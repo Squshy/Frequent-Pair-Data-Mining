cf6d37bcc08702d280fc20ae60d15effbe72d5f3
Squshy <36893334+Squshy@users.noreply.github.com>
2021-02-27 17:07:04 -0500

    comments and cleaning up before PCY

--- helper.py
+++ helper.py
@@ -3,30 +3,11 @@ import pandas as pd
 import numpy as np
 from itertools import combinations
 
+# Reads a test file and returns its data in an array
 def GetItemsetFromFile(file):
   return np.asarray([i.strip().split() for i in open(file, 'r').readlines()])
 
-def SaveDataToFile(data, support, filename):
-  try:
-    os.remove(filename)
-  except OSError:
-    pass
-  file = open(filename, "w")
-  i = 0
-  for clump in data:
-    file.write("{")
-    first = True
-    for item in clump:
-      if first:
-        first = False
-        file.write(str(item))
-      else:
-        file.write(", ")
-        file.write(str(item))
-    file.write("} Support: %.2f\n" % support[i])
-    i += 1
-  file.close()
-
+# Takes an array of support values and the data values and appends them together
 def CreateSupportList(data, support):
   i = 0
   new_list = []
@@ -39,6 +20,7 @@ def CreateSupportList(data, support):
     i+=1
   return new_list
 
+# Saves a Pandas DataFrame to an html file
 def SaveDataFrameToHTMLFile(df, filename):
   html = df.to_html()
   filepath = "./report/table_files/" + filename + ".html"
@@ -50,9 +32,12 @@ def SaveDataFrameToHTMLFile(df, filename):
   file.write(html)
   file.close()
 
+# Get subsets of data
 def rSubset(data, r):
   return list(combinations(data, r))
 
+# Gets x % of data from the data passed
+# If you pass 0.2, you get 20% of the data returned starting from the beginning of the data
 def GetSubsectionOfData(data, percent):
   data_len = len(data)
   amount_of_data = round(data_len * percent)

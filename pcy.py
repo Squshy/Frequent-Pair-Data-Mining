c02bcf66adce1d9ed5a469fd8bb5f3524705b32f
Squshy <36893334+Squshy@users.noreply.github.com>
2021-02-27 20:56:52 -0500

    PCY working for pairs

--- pcy.py
+++ pcy.py
@@ -36,6 +36,43 @@ def PassOne(data, support):
         hash_table[key] = 1
       else: 
         hash_table[key] += 1
+  occurences, frequent_items = GetFrequentItems(occurences, support * len(data))
+  return hash_table, occurences, frequent_items
 
-  # Return a list of frequent items
-  return GetFrequentItems(occurences, support * len(data))
+def DetermineFrequentBuckets(hash_table, support):
+  freq_buckets = {}
+  for bucket in hash_table:
+    if hash_table[bucket] >= support:
+      freq_buckets[bucket] = 1
+  return freq_buckets
+
+def PassTwo(data, frequent_items, support, bitmap):
+  frequent_candidates = {}
+
+  for line in data:
+    if(len(line) >= 2):
+      temp_ = rSubset(line, 2)  # Create sets of pairs from the line
+      # For every pair of items in the current list of item pairs
+      for item_set in temp_:
+        item_set = tuple(sorted(item_set))  # sort the tuple
+        is_frequent = True  # set a frequent flag to true
+        # For every individual item in the pear
+        for item in item_set:
+          # If the item is not in the previously counted frequent list
+          if item not in frequent_items:
+            is_frequent = False     # Set the frequent flag to false
+        # If the pair is frequent
+        if is_frequent == True:
+          key = HashFunction(item_set, len(data))
+          if key in bitmap:
+            if item_set not in frequent_candidates:
+              frequent_candidates[item_set] = 1
+            else: 
+              frequent_candidates[item_set] += 1
+  return GetFrequentItems(frequent_candidates, support * len(data))
+
+def PCY(data, support, k):
+  hash_table, occurences, freq_items = PassOne(data, support)
+  bitmap = DetermineFrequentBuckets(hash_table, support)
+  pair_occurences, freq_pairs = PassTwo(data, freq_items, support, bitmap)
+  return freq_pairs
